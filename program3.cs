e224a68f424cc25c4a9dcb33a824423c09ea1a17
Nowicki41 <56186033+Nowicki41@users.noreply.github.com>
2019-10-20 09:23:29 +0200

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              for ( char znak = 'A'; znak <= 'Z'; znak++) {
+              Console.WriteLine("{0}", znak);
+              */
+         //Napisz program który za pomocą insturkcji while dla dancyh wartości x zmieniejących się w przedziale od 0 do 10 oblicza wartość dla funkcji y=4x
+            int x = 0;
+            while (x <= 10)
+            {
+                int y = 4 * x;
+                x++;
+                Console.WriteLine(y);
+
+              
+            }
+            x = 0;
+            int suma = 0;
+            while(x<=50)
+            {
+                suma = suma+x;
+                x++;
+
+              
+
+            }
+            /*
+            do
+            {Console.WriteLine("Podaj wiek: ");
+            age = int.Parse(Console.ReadLine());
+            while(age < 1 || age >120) {
+
+            }
+            }
+             */
+
+            // Wyświetl talbiczkę mnożenia za pomocą dwóch pętli do while
+
+            {
+               
+              }
+
+                }
+                
+
+            }
+         
+    //napisz program który za pomocą pętli while dla danych wartości x zmieniający się w przedziale od 1 do 50 obliczy ich sume
+
+            //napisz program który wyświetli duże liter alfabetu od A do Z oraz od Z do A  z wykorzystaniem pętli for np.A,B,C
+    //podaj wiek od 1 do 120 i sprawdź czy poprawnie wpisał  
+
+        }
+
+    
+

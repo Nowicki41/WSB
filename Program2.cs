7eb92b60d8c3cbff7efb4774abd18fba763f66bb
Nowicki41 <56186033+Nowicki41@users.noreply.github.com>
2019-11-10 09:30:41 +0100

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typy_wartościowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = a;
+            a--;
+            b++;
+            Console.WriteLine(a); //9
+            Console.WriteLine(b); // 11
+
+            //typ referencyjny
+            int[] tabA = { 1, 2, 3 };
+            int[] tabB = tabA;
+            tabA[0] = 100;
+            tabB[tabB.Length - 1] = 300;
+            Console.WriteLine("\ntabA");
+            foreach (int y in tabA)
+            {
+                Console.Write("{0}",y);
+            }
+            //Kopiowanie tablic
+            int[] tab = { 1, 2, 3, 4, 5 };
+            int[] tabCopy = new int[7];
+            Console.WriteLine("tabCopy");
+            foreach (int item in tabCopy)
+            {
+                Console.Write("{0}", item);
+            }
+            tab.CopyTo(tabCopy, 0);
+            foreach (int item in tabCopy)
+            {
+                Console.Write("{0}", item);
+            }
+            Array.Copy(tab, tabCopy, 5);
+            foreach (int item in tabCopy)
+            {
+                Console.Write("{0}", item);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Wyjatki.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyjątki1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            uint number = 0;
+            while (true)
+            {
+                Console.Write("Wproadź liczbę całkowitą:");
+                x = Console.ReadLine();
+                try
+                {
+                     number = uint.Parse(x);
+                    break;
+                }
+                catch (SystemException e)
+                {
+                    Console.WriteLine("error:{0}", e.Message);
+                }
+            }
+ 
+            try
+            {
+                 number = uint.Parse(x);
+            }
+            catch (SystemException e)
+
+            
+            {
+                Console.WriteLine("error:{0}",e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
